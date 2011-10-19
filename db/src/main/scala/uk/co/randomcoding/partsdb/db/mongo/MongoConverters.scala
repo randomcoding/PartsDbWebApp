25cbfea88d7cc7555849d33484c758a7bae74d2e
randomcoder <randomcoder@randomcoding.co.uk>
2011-10-19 08:03:28 +0100

    Updated ignore file to exclude all project directories and added scaladoc to MongoConverters.scala.

--- db/src/main/scala/uk/co/randomcoding/partsdb/db/mongo/MongoConverters.scala
+++ db/src/main/scala/uk/co/randomcoding/partsdb/db/mongo/MongoConverters.scala
@@ -18,12 +18,29 @@ object MongoConverters {
 
   private implicit val formats = DefaultFormats
 
+  /**
+   * Converts from a [[com.mongodb.casbah.Imports.DBObject]] to a class defined within the application codebase.
+   *
+   * This is usually used to convert from the results returned from a query into types objects.
+   *
+   * @param mongoObject The object from the MongoDB database to convert
+   * @tparam T The type to convert the object into
+   * @return The converted object. This will be of type '''`T`'''
+   */
   implicit def convertFromMongoDbObject[T](mongoObject: DBObject)(implicit mf: Manifest[T]): T = {
     val mObj = mongoObject.removeField("_id")
     val jsonString: String = mongoObject.toString
     read(jsonString)
   }
 
+  /**
+   * Converts a library object to a [[com.mongodb.casbah.Imports.DBObject]].
+   *
+   * This is used in the process of storing items in the database
+   *
+   * @param t The object to convert to a [[com.mongodb.casbah.Imports.DBObject]]. I think this '''must''' be a '''`case class`''' for this conversion to work
+   * @return The [[com.mongodb.casbah.Imports.DBObject]]
+   */
   implicit def convertToMongoDbObject[T <: AnyRef](t: T): DBObject = {
     com.mongodb.util.JSON.parse(t).asInstanceOf[DBObject]
   }
