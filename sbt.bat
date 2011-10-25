f27307b7c52df3ea70732c09064f54199c488c46
Jane Rowe <jane@innocentbystanders.com>
2011-10-25 21:24:15 +0100

    Added Java option to sbt.bat

--- sbt.bat
+++ sbt.bat
@@ -1,6 +1,6 @@
 
 set SBT_VER=0.11
-set JAVA_OPTS="-Dfile.encoding=UTF8 -Xmx512M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m"
+set JAVA_OPTS="-d32 -Dfile.encoding=UTF8 -Xmx512M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m"
 set LAUNCH_DIR=%~dp0
 
 java %JAVA_OPTS% -jar "%LAUNCH_DIR%sbt-%SBT_VER%-launch.jar " %*
