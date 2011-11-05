5400e991466325146094383f75c5fca88db31252
Jane Rowe <jane@innocentbystanders.com>
2011-11-05 16:06:10 +0000

    Added cost to the Part object, and a minor text change to the part web page to check its working

--- core/src/main/scala/uk/co/randomcoding/partsdb/core/part/Part.scala
+++ core/src/main/scala/uk/co/randomcoding/partsdb/core/part/Part.scala
@@ -10,8 +10,9 @@ import uk.co.randomcoding.partsdb.core.vehicle.Vehicle
  * @param partId The [[uk.co.randomcoding.partsdb.core.id.Identifier]] of this part. This is used for internal referencing of part objects from other entities.
  * @param partName The short (friendly) name of this part
  * @param vehicles The set of vehicles this part can be used for
+ * @param cost The cost of the part
  *
  * @author JMRowe <>
  *
  */
-case class Part(val partId: Identifier, val partName: String, val vehicles: Set[Vehicle])
+case class Part(val partId: Identifier, val partName: String, val vehicles: Set[Vehicle], val cost: Double)
