c0e4da0e02579b52430854c8f4ec65364d95d65d
randomcoder <randomcoder@randomcoding.co.uk>
2011-11-06 08:19:32 +0000

    re #7: Added tests for modify operations.

--- db/src/test/scala/uk/co/randomcoding/partsdb/db/mongo/MongoUpdateAccessModifyTest.scala
+++ db/src/test/scala/uk/co/randomcoding/partsdb/db/mongo/MongoUpdateAccessModifyTest.scala
@@ -32,15 +32,40 @@ class MongoUpdateAccessModifyTest extends MongoDbTestBase with ShouldMatchers {
   }
 
   test("Modify of Address already added to database correctly modifies object") {
-    fail("Not Implemented Yet")
+    val address1 = Address(Identifier(9876), "Addr1", "Long Addr 1", "UK")
+    mongoAccess add address1 should be(true)
+
+    val address2 = Address(Identifier(9876), "Addr1", "Long Addr 1 Modified", "UK")
+    mongoAccess modify address2 should be(true)
+
+    findInDatabase[Address]("addressId", 9876) should be(address2)
   }
 
-  test("Multiple modifications to the same Addres result in the correct Address in the database") {
-    fail("Not Implemented Yet")
+  test("Multiple modifications to the same Address result in the correct Address in the database") {
+    val address1 = Address(Identifier(9876), "Addr1", "Long Addr 1", "UK")
+    mongoAccess add address1 should be(true)
+
+    val address2 = Address(Identifier(9876), "Addr1", "Long Addr 1 Modified", "UK")
+    mongoAccess modify address2 should be(true)
+
+    val address3 = Address(Identifier(9876), "Addr2", "Long Addr 1 Modified", "UK")
+    mongoAccess modify address3 should be(true)
+
+    val address4 = Address(Identifier(9876), "Addr2", "Long Addr 2 Modified", "USA")
+    mongoAccess modify address4 should be(true)
+
+    findInDatabase[Address]("addressId", 9876) should be(address4)
   }
 
   test("Modify called on Address that is not is database does not add it to database") {
-    fail("Not Implemented Yet")
+    val address1 = Address(Identifier(9876), "Addr1", "Long Addr 1", "UK")
+    mongoAccess add address1 should be(true)
+
+    val address2 = Address(Identifier(98765), "Addr1", "Long Addr 1", "UK")
+    mongoAccess modify address2 should be(false)
+
+    findInDatabase[Address]("addressId", 9876) should be(address1)
+    findInDatabase[Address]("addressId", 98765) should be(None)
   }
 
 }
