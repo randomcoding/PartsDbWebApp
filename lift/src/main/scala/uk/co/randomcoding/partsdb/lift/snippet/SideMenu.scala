1db1cd2288ee0c5977b46a9c1c04c38daebb35bf
randomcoder <randomcoder@randomcoding.co.uk>
2011-11-22 08:23:11 +0000

    re #3: Added css styled buttons to customer page and side menu.

---
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/snippet/SideMenu.scala
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+package uk.co.randomcoding.partsdb.lift.snippet
+
+import net.liftweb.http.S
+import net.liftweb.sitemap._
+import scala.xml._
+import net.liftweb.common.Logger
+
+/**
+ * @author RandomCoder <randomcoder@randomcoding.co.uk>
+ */
+class SideMenu extends Logger {
+  def builder: NodeSeq = {
+    info("Called builder")
+
+    S.request.map(req => {
+      val menu = req.buildMenu
+      info("Menu: %s".format(menu))
+      info("Menu Lines: %s".format(menu.lines))
+      menu.lines match {
+        case Nil => Text("No Navigation Defined.")
+        case x :: xs => <ul id="navlist">{ (x :: xs).flatMap(buildANavItem(_)) }</ul>
+      }
+    }).openOr(Text("No Navigation Defined."))
+  }
+
+  private def buildANavItem(i: MenuItem) = i match {
+    case MenuItem(text, uri, _, true, _, _) => (<li><a href={ uri } class="active button"><span>{ text }</span></a></li>)
+    case MenuItem(text, uri, _, _, true, _) => (<li><a href={ uri } class="active button"><span>{ text }</span></a></li>)
+    case MenuItem(text, uri, _, _, _, _) => (<li><a class="button" href={ uri }><span>{ text }</span></a></li>)
+  }
+}
