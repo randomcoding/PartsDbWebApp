09396ac9f1b5f78327287b8f67db572395509153
randomcoder <randomcoder@randomcoding.co.uk>
2011-11-23 08:07:38 +0000

    re #3: MOdified links to point to add/edit pages with a url query parameter indicating the type of entity.
    Also added new Helper Object to provide common implementation of element
    generation and transformations.

--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/snippet/AddOrEdit.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/snippet/AddOrEdit.scala
@@ -3,22 +3,58 @@
  */
 package uk.co.randomcoding.partsdb.lift.snippet
 
-import net.liftweb._
-import http.{ StatefulSnippet, S, SHtml }
-import util.Helpers._
+import scala.xml.NodeSeq
+
+import net.liftweb.http.SHtml.ElemAttr.pairToBasic
+import net.liftweb.http.{ SHtml, S }
+import net.liftweb.util.Helpers.{ strToSuperArrowAssoc, strToCssBindPromoter }
+
+import uk.co.randomcoding.partsdb.lift.util.TransformHelpers._
 
 /**
  * Simple snippet that displays Add or Edit buttons.
  *
+ * This snippet expects a parametr called ''entityType'' that determines the
+ * text that is displayed and the link that is generated.
+ *
+ * It is also expecting `<div>` elements with two specific ids:
+ *  * `add`
+ *  * `edit`
+ * Which are transformed into the add and edit links respectively.
+ *
+ * == Example
+ * {{{
+ * <div class="lift:AddOrEdit?entityType=Customer">
+ *   <div id="add">Add Link is created here</div>
+ *   <div id="edit">Edit Link is created here</div>
+ * </div>
+ * }}}
+ *
+ * will create the html
+ *
+ * {{{
+ * <div class="lift:AddOrEdit?entityType=Customer">
+ *   <a href="add?entityType=Customer" class="button"><span>Add Customer</span></a></div>
+ *   <a href="edit?entityType=Customer" class="button"><span>Edit Customer</span></a></div>
+ * </div>
+ * }}}
  * @author RandomCoder <randomcoder@randomcoding.co.uk>
  */
 class AddOrEdit {
+
+  /**
+   * Transforms the `#add` and `#edit` divs into links with `add/edit?entityType=...` targets.
+   *
+   * @param entityType The type of entity that is to be added or edited. This value is used ''as is''
+   * so should not contain spaces and any capitalisation is preserved.
+   */
   def render = {
     val entityType = S.attr("entityType") openOr ("Unspecified")
     val addText = "Add " + entityType
     val editText = "View or Edit %s".format(entityType)
 
-    "#add" #> SHtml.link("add" + entityType, () => Unit, <span>{ addText }</span>, "class" -> "button") &
-      "#edit" #> SHtml.link("edit" + entityType, () => Unit, <span>{ editText }</span>, "class" -> "button")
+    "#add" #> buttonLink("add?entityType=%s".format(entityType), addText) &
+      "#edit" #> buttonLink("edit?entityType=%s".format(entityType), editText)
   }
+
 }
---
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/TransformHelpers.scala
@@ -0,0 +1,50 @@
+/**
+ *
+ */
+package uk.co.randomcoding.partsdb.lift.util
+
+import scala.xml.{ Text, NodeSeq }
+
+import net.liftweb.http.SHtml.{ span, link, ElemAttr }
+import net.liftweb.http.js.JsCmds.Noop
+
+/**
+ * Provides common helper functions for generating elements for transformations
+ *
+ * @author RandomCoder <randomcoder@randomcoding.co.uk>
+ */
+object TransformHelpers {
+
+  /**
+   * Creates a link that has the additional class of button and an inner `<span>` element. This will cause it to be rendered as a button by the default css.
+   *
+   * @param linkText The text that it to be inside the button
+   * @param linkTarget The url of the destination of the link
+   * @return a [[scala.xml.NodeSeq]] for a link with an inner span and the class attribute of the `<a>` element set to `button`
+   */
+  def buttonLink(linkText: String, linkTarget: String): NodeSeq = {
+    attrLink(linkTarget, linkText, "class" -> "button")
+  }
+
+  /**
+   * Creates a plain link with an inner span element.
+   *
+   * @param linkText The text that it to be inside the button
+   * @param linkTarget The url of the destination of the link
+   */
+  def plainLink(linkText: String, linkTarget: String): NodeSeq = {
+    attrLink(linkTarget, linkText)
+  }
+
+  /**
+   * Creates a link with an inner span element and the given attributes.
+   *
+   * @param linkText The text that it to be inside the button
+   * @param linkTarget The url of the destination of the link
+   * @param linkAttrs A varargs list of `"attributeName" -> "attributeValue"` pairs to add to the `<a>` element
+   * @return a [[scala.xml.NodeSeq]] for a link with an inner span and the given element attributes set on the `<a>` element
+   */
+  def attrLink(linkText: String, linkTarget: String, linkAttrs: ElemAttr*): NodeSeq = {
+    link(linkTarget, () => Unit, span(Text(linkText), Noop), linkAttrs: _*)
+  }
+}
