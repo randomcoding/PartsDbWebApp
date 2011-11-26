e50ece8bf79c5e1625552be7728c91216687af04
randomcoder <randomcoder@randomcoding.co.uk>
2011-11-26 11:16:16 +0000

    re #3: Added empty test stubs for address matching.

---
+++ core/src/test/scala/uk/co/randomcoding/partsdb/core/address/AddressMatchTest/AddressMatchingTest.scala
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package uk.co.randomcoding.partsdb.core.address.AddressMatchTest
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+/**
+ * @author RandomCoder <randomcoder@randomcoding.co.uk>
+ */
+class AddressMatchingTest extends FunSuite with ShouldMatchers {
+  test("Test can match a UK address string") {
+    pending
+  }
+
+  test("Test can match a UK address as list of strings") {
+    pending
+  }
+
+}
