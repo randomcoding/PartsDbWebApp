cc3a495ffedd4e0a8732a4a09345ce24c94021fa
randomcoder <randomcoder@randomcoding.co.uk>
2011-12-11 13:08:59 +0000

    re #9: Added user display to DisplayExisting, styled password function to TransformHelpers and convenience delegate to ErrorDisplay.

--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/snippet/ErrorDisplay.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/snippet/ErrorDisplay.scala
@@ -16,8 +16,13 @@ trait ErrorDisplay {
    * @param formId The id of the element on the form web page to display the error at.
    * @param errorMessage The text of the error message to display
    */
-  def displayError(formId: String, errorMessage: String): Unit = {
-    S.error(formId, errorMessage)
-  }
+  def displayError(formId: String, errorMessage: String): Unit = S.error(formId, errorMessage)
+
+  /**
+   * Delegate to [[uk.co.randomcoding.partsdb.lift.util.snippet.ErrorDisplay.displayError(String, String)]]
+   *
+   * @param A `Tuple2[String, String]` which is mapped to the parameters of the delegate method
+   */
+  def displayError(error: (String, String)): Unit = displayError(error._1, error._2)
 
 }
