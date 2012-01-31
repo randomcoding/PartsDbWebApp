65db15119466ca9e72c132d99fb2f7567fceaae9
randomcoder <randomcoder@randomcoding.co.uk>
2012-01-31 23:01:01 +0000

    re #29: Added tests and corrected implementation of Address functionality.

--- core/src/main/scala/uk/co/randomcoding/partsdb/core/address/Address.scala
+++ core/src/main/scala/uk/co/randomcoding/partsdb/core/address/Address.scala
@@ -20,22 +20,75 @@ class Address private () extends MongoRecord[Address] with ObjectIdPk[Address] {
   object shortName extends StringField(this, 50)
   object addressText extends StringField(this, 300)
   object country extends StringField(this, 50)
+
+  override def equals(that: Any): Boolean = {
+    that.isInstanceOf[Address] match {
+      case false => false
+      case true => {
+        val other = that.asInstanceOf[Address]
+        shortName.get == other.shortName.get && addressText.get == other.addressText.get && country.get == other.country.get
+      }
+    }
+  }
+
+  override def hashCode: Int = getClass.hashCode + shortName.get.hashCode + addressText.get.hashCode + country.get.hashCode
 }
 
 object Address extends Address with MongoMetaRecord[Address] {
   import org.bson.types.ObjectId
-  
+
+  /**
+   * Get the address with the given id.
+   *
+   * @return An Option containing the address that is found, or `None` if there is no address with the given identifier
+   */
   def findById(oid: ObjectId) = Address where (_.id eqs oid) get
-  
-  def findByShortName(shortName: String) = Address where (_.shortName eqs shortName) fetch
-  
+
+  /**
+   * Find all addresses that have the given short name.
+   *
+   * As we try to ensure that all short names are unique, this '''should''' only ever return at most a single result
+   */
+  def findNamed(shortName: String) = Address where (_.shortName eqs shortName) fetch
+
+  /**
+   * Finds all addresses with the given `addressText`.
+   *
+   * The likelihood is that this will return at most a single result.
+   */
   def findByAddressText(addressText: String) = Address where (_.addressText eqs addressText) fetch
-  
-  def remove(shortName: String) = findByShortName(shortName) map (_.delete_!) distinct
-  
+
+  /**
+   * Remove the address with the given `shortName`
+   */
+  def remove(shortName: String) = findNamed(shortName) map (_.delete_!) distinct
+
+  /**
+   * Modify '''''all''''' the properties of the address with the given `ObjectId`
+   *
+   * If you don't want to change a value, then provide the original value
+   *
+   * @param newShortName The new short name to set
+   * @param newAddressText The new address text to set
+   * @param newCountry The new country to set
+   */
   def modify(oid: ObjectId, newShortName: String, newAddressText: String, newCountry: String) = {
-    Address where (_.id eqs oid) modify (_.shortName setTo newShortName) and (_.addressText setTo newAddressText) and (_.country setTo newCountry) updateMulti
+    Address.where(_.id eqs oid).modify(_.shortName setTo newShortName) and (_.addressText setTo newAddressText) and (_.country setTo newCountry) updateMulti
   }
-  
-  def add(shortName: String, addressText: String, country: String) = Address.createRecord.shortName(shortName).addressText(addressText).country(country).save
+
+  /**
+   * Add a new Address unless there is an address with the same `shortName` or `addressText` in the database
+   *
+   * @return An `Option[Address]`, populated with the new `Address` if it was added successfully, or None if it was not.
+   */
+  def add(shortName: String, addressText: String, country: String) = (findNamed(shortName) ++ findByAddressText(addressText)) match {
+    case Nil => {
+      Address.createRecord.shortName(shortName).addressText(addressText).country(country).save match {
+        case addr: Address => Some(addr)
+        case _ => None
+      }
+    }
+    case _ => None
+  }
+
 }
--- db/src/test/scala/uk/co/randomcoding/partsdb/db/mongo/AddressRecordTest.scala
+++ db/src/test/scala/uk/co/randomcoding/partsdb/db/mongo/AddressRecordTest.scala
@@ -6,45 +6,96 @@ package uk.co.randomcoding.partsdb.db.mongo
 import com.foursquare.rogue.Rogue._
 import uk.co.randomcoding.partsdb.core.vehicle.Vehicle
 import uk.co.randomcoding.partsdb.core.vehicle.Vehicle._
+import org.bson.types.ObjectId
 
 /**
  * @author RandomCoder <randomcoder@randomcoding.co.uk>
  */
 class AddressRecordTest extends MongoDbTestBase {
+  import uk.co.randomcoding.partsdb.core.address.Address._
+  import uk.co.randomcoding.partsdb.core.address.Address
 
   override val dbName = "AddressRecordTest"
 
   test("Adding a single address works ok") {
-    pending
+    val addr = add("Addr1", "An Address", "UK")
+
+    val expectedAddress = createRecord.shortName("Addr1").addressText("An Address").country("UK")
+
+    addr should be(Some(expectedAddress))
+
+    findNamed("Addr1") should be(List(expectedAddress))
+
+    findById(addr.get.id.get) should be(Some(expectedAddress))
   }
 
   test("Adding the same address more than once does not result in a duplication, or a change of underlying id") {
-    pending
+    val addr = add("Addr1", "An Address", "UK")
+    addr should be('defined)
+    add("Addr1", "An Address", "UK") should be('empty)
+
+    val expectedAddress = createRecord.shortName("Addr1").addressText("An Address").country("UK")
+
+    (Address where (_.id exists true) fetch) should be(List(expectedAddress))
+
+    findNamed("Addr1")(0).id.get should be(addr.get.id.get)
   }
 
   test("Removing an address") {
-    pending
+    val addr1 = add("Addr1", "An Address", "UK").get
+    val addr2 = add("Addr2", "Another Address", "UK")
+
+    val addr1Id = addr1.id.get
+    remove("Addr1") should be(List(true))
+
+    findNamed("Addr1") should be('empty)
+    findById(addr1Id) should be('empty)
+
+    (Address where (_.id exists true) fetch) should be(List(addr2.get))
   }
 
   test("Removing an address from an empty database") {
-    pending
+    remove("Addr123") should be(Nil)
+    remove("Addr456") should be(Nil)
   }
 
   test("Find an address that is present in the database") {
-    // by short name, address text and oid
-    pending
+    val addr1 = add("Address1", "An Address Text", "UK").get
+
+    findNamed("Address1") should be(List(addr1))
+
+    findById(addr1.id.get) should be(Some(addr1))
+
+    findByAddressText("An Address Text") should be(List(addr1))
   }
 
   test("Find an address that is not present in the database") {
-    // by short name, address text and oid
-    pending
+    val addr1 = add("Address1", "An Address Text", "UK").get
+
+    findNamed("Address2") should be(Nil)
+
+    findById(new ObjectId("4f2871f4231823ddb82a080c")) should be('empty)
+
+    findByAddressText("Another Address Text") should be(Nil)
   }
 
   test("Modify an address") {
-    pending
+    val origAddr = add("Address 1", "An Address Text", "UK").get
+    val origId = origAddr.id.get
+
+    modify(origAddr.id.get, "Address 1-1", "Modified Address Text", "United Kingdom")
+
+    findNamed("Address 1") should be(Nil)
+    findNamed("Address 1-1") should be(List(createRecord.shortName("Address 1-1").addressText("Modified Address Text").country("United Kingdom")))
   }
 
   test("Modfy an address does not modify its object id") {
-    pending
+    val origAddr = add("Address 1", "An Address Text", "UK").get
+    val origId = origAddr.id.get
+
+    modify(origAddr.id.get, "Address 1-1", "Modified Address Text", "United Kingdom")
+
+    findNamed("Address 1") should be(Nil)
+    findById(origId) should be(Some(createRecord.shortName("Address 1-1").addressText("Modified Address Text").country("United Kingdom")))
   }
 }
