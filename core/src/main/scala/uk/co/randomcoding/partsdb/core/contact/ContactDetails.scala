1d8410885f83b41b408fbe7ade7611a14b075b41
randomcoder <randomcoder@randomcoding.co.uk>
2012-02-01 22:59:49 +0000

    re #29: Changed Contact Details to be a full db object.

--- core/src/main/scala/uk/co/randomcoding/partsdb/core/contact/ContactDetails.scala
+++ core/src/main/scala/uk/co/randomcoding/partsdb/core/contact/ContactDetails.scala
@@ -4,21 +4,40 @@
 package uk.co.randomcoding.partsdb.core.contact
 
 import net.liftweb.mongodb.record.field._
-import net.liftweb.mongodb.record.{ MongoRecord, MongoMetaRecord }
+import net.liftweb.mongodb.record._
 import net.liftweb.record.field._
-
+import scala.math.Ordering.String
 /**
  * Some contact details for a customer or supplier.
- *
- * This associates a Contact's name to
- *
- * @constructor Create a new Contact details instance.
- *
- * @param contacts A map of contact name -> contact number(s)
  *
  * @author RandomCoder <randomcoder@randomcoding.co.uk>
  *
  */
-case class ContactDetails(val contactName: String, val phoneNumbers: Option[List[Phone]] = None, val mobileNumbers: Option[List[Mobile]] = None, val emailAddresses: Option[List[Email]] = None)
+class ContactDetails private () extends MongoRecord[ContactDetails] with ObjectIdPk[ContactDetails] {
+  def meta = ContactDetails
 
-object NullContactDetails extends ContactDetails("")
+  object contactName extends StringField(this, 50)
+  object phoneNumbers extends StringField(this, 50)
+  object mobileNumbers extends StringField(this, 50)
+  object emailAddresses extends StringField(this, 50)
+
+  override def equals(that: Any): Boolean = {
+    that.isInstanceOf[ContactDetails] match {
+      case false => false
+      case true => {
+        val other = that.asInstanceOf[ContactDetails]
+        contactName == other.contactName &&
+          phoneNumbers.get == other.phoneNumbers.get &&
+          mobileNumbers.get == other.mobileNumbers.get &&
+          emailAddresses.get == other.emailAddresses.get
+
+      }
+    }
+  }
+
+  def listCompare(list1: List[String], list2: List[String]): Boolean = list1.sorted(String) == list2.sorted(String)
+}
+
+object ContactDetails extends ContactDetails with MongoMetaRecord[ContactDetails] {
+
+}
