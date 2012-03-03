1aa0618da1b8c6609bdd699bd22cd541ac84ac8b
randomcoder <randomcoder@randomcoding.co.uk>
2012-03-03 19:09:44 +0000

    re #45: Added button to display the transaction to each result row.

--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/TransactionSummaryDisplay.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/TransactionSummaryDisplay.scala
@@ -9,15 +9,16 @@ import uk.co.randomcoding.partsdb.core.transaction.Transaction
 import net.liftweb.util.Helpers._
 import scala.xml.Text
 import org.joda.time.DateTime
+import net.liftweb.http.SHtml._
 
 /**
  * @author RandomCoder <randomcoder@randomcoding.co.uk>
  */
 object TransactionSummaryDisplay {
 
-  def apply(transactions: Seq[Transaction]) = {
-    transactions map (transaction =>
-      "#transactionName" #> Text("Transaction Name TBD") &
-        "#transactionStarted" #> Text(new DateTime(transaction.creationDate.get).toString("dd/MM/yyyy")))
-  }
+  def apply(transactions: Seq[Transaction]) = transactions map (transaction => {
+    "#transactionName" #> Text("Transaction Name TBD") &
+      "#transactionStarted" #> Text(new DateTime(transaction.creationDate.get).toString("dd/MM/yyyy")) &
+      "#display" #> link("/display/Transaction?id=%s".format(transaction.id.get), () => (), Text("Display"))
+  })
 }
