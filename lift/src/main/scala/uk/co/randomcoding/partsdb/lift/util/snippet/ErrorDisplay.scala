5fafda63f5dc0d134385553041bf11a0a49a156e
randomcoder <randomcoder@randomcoding.co.uk>
2012-03-07 23:33:57 +0000

    re #30: Refactored error message display trait to force use of a single error element id as there is only on used via lift:Messages.

--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/snippet/ErrorDisplay.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/snippet/ErrorDisplay.scala
@@ -9,21 +9,35 @@ import net.liftweb.http.S
  * @author RandomCoder <randomcoder@randomcoding.co.uk>
  */
 trait ErrorDisplay {
-
+  private[this] val ERROR_MESSAGE_ELEMENT_ID = "errorMessages"
   /**
    * Displays an error message from a snippet
    *
    * @param formId The id of the element on the form web page to display the error at.
    * @param errorMessage The text of the error message to display
    */
-  def displayError(formId: String, errorMessage: String): Unit = S.error(formId, errorMessage)
+  @deprecated("Use displayError(String) instead", "0.1")
+  def displayError(formId: String, errorMessage: String): Unit = displayError(errorMessage)
+
+  /**
+   * Displays an error message from a snippet
+   *
+   * @param errorMessage The text of the error message to display
+   */
+  def displayError(errorMessage: String): Unit = S.error(ERROR_MESSAGE_ELEMENT_ID, errorMessage)
+
+  /**
+   * Display a series of error messages
+   */
+  def displayErrors(errorMessages: String*): Unit = errorMessages foreach displayError
 
   /**
    * Delegate to [[uk.co.randomcoding.partsdb.lift.util.snippet.ErrorDisplay#displayError(String,String)]]
    *
    * @param errors One or more `Tuple2[String, String]` which are mapped to the parameters of the delegated method
    */
-  def displayError(errors: (String, String)*): Unit = errors foreach { error => displayError(error._1, error._2) }
+  @deprecated("Use displayErrors(String*) instead", "0.1")
+  def displayError(errors: (String, String)*): Unit = errors foreach { error => displayError(error._2) }
 
   /**
    * Remove all errors from the display
