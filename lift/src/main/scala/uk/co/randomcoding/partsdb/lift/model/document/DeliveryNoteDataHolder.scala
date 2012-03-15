7801dea96b81cd0b89c68fffe207d026eeb76b5e
randomcoder <randomcoder@randomcoding.co.uk>
2012-03-15 23:37:30 +0000

    re #51: Added selection of order delivery address to delivery page.
    This also should update the carriage, and P/O reference on change.
    
    Additionally added address finding from all delivery notes for the
    current customer and rendering of the new address section.

--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/model/document/DeliveryNoteDataHolder.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/model/document/DeliveryNoteDataHolder.scala
@@ -4,6 +4,9 @@
 package uk.co.randomcoding.partsdb.lift.model.document
 
 import net.liftweb.util.Cell
+import net.liftweb.util.ValueCell
+import uk.co.randomcoding.partsdb.core.document.Document
+import uk.co.randomcoding.partsdb.core.address.Address
 
 /**
  * @author RandomCoder <randomcoder@randomcoding.co.uk>
@@ -15,4 +18,31 @@ class DeliveryNoteDataHolder extends DocumentDataHolder with LineItemsDataHolder
   override val preTaxTotal: Cell[Double] = itemsPreTaxSubTotal.lift(carriageCell)(_ + _)
 
   override val lineItemsSubTotalCell: Cell[Double] = itemsPreTaxSubTotal.lift(_ + 0.0d)
+
+  val selectedOrderCell = ValueCell[Option[Document]](None)
+
+  val poReferenceCell = selectedOrderCell.lift(_ match {
+    case Some(d) => d.customerPoReference.get
+    case _ => ""
+  })
+
+  private val deliveryAddressCell = ValueCell[Option[Address]](None)
+
+  // Accessor functions
+
+  def selectedOrder = selectedOrderCell.get
+
+  /**
+   * Setter for the value of the selected order cell.
+   *
+   * This will also update the carriage value in the parent [[uk.co.randomcoding.partsdb.lift.model.document.DocumentDataHolder]]
+   */
+  def selectedOrder_=(order: Option[Document]) = {
+    selectedOrderCell set order
+    carriage = if (order isDefined) order.get.carriage.get else 0.0d
+  }
+
+  def deliveryAddress = deliveryAddressCell.get
+
+  def deliveryAddress_=(addr: Option[Address]) = deliveryAddressCell set addr
 }
--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/snippet/AddEditDelivery.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/snippet/AddEditDelivery.scala
@@ -9,17 +9,55 @@ import net.liftweb.util.Helpers._
 import net.liftweb.common.Full
 import uk.co.randomcoding.partsdb.core.document.Document
 import org.bson.types.ObjectId
-import uk.co.randomcoding.partsdb.lift.util.snippet.TransactionSnippet
+import uk.co.randomcoding.partsdb.lift.util.snippet._
+import uk.co.randomcoding.partsdb.lift.util.DateHelpers._
 import scala.xml.Text
 import uk.co.randomcoding.partsdb.core.document.DocumentType
+import uk.co.randomcoding.partsdb.lift.snippet.js.JsScripts
+import uk.co.randomcoding.partsdb.lift.util.snippet.display.DocumentDataHolderTotalsDisplay
+import uk.co.randomcoding.partsdb.lift.model.document.DeliveryNoteDataHolder
+import uk.co.randomcoding.partsdb.core.document.LineItem
+import net.liftweb.http.WiringUI
+import uk.co.randomcoding.partsdb.lift.util.TransformHelpers._
+import uk.co.randomcoding.partsdb.core.transaction.Transaction
+import com.foursquare.rogue.Rogue._
+import uk.co.randomcoding.partsdb.core.address.Address
 
 /**
  * @author RandomCoder <randomcoder@randomcoding.co.uk>
  */
-class AddEditDelivery extends StatefulValidatingErrorDisplaySnippet with TransactionSnippet {
+class AddEditDelivery extends StatefulValidatingErrorDisplaySnippet with TransactionSnippet with AvailableLineItemsDisplay with AllLineItemsSnippet with DocumentDataHolderTotalsDisplay with AddressSnippet {
 
-  private[this] lazy val deliveryNotes = documentsOfType(DocumentType.DeliveryNote)
-  private[this] lazy val orders = documentsOfType(DocumentType.Order)
+  override val dataHolder = new DeliveryNoteDataHolder
+
+  override var addressText: String = ""
+  override var addressCountry: String = ""
+
+  private[this] lazy val previousDeliveryNotes = documentsOfType(DocumentType.DeliveryNote)
+  private[this] lazy val orders = documentsOfType(DocumentType.Order).toList
+
+  private[this] lazy val ordersSelection = (None, "Select Order") :: (orders map (order => (Some(order), textForOrder(order))))
+
+  private[this] val itemsOrdered = orders flatMap (_.lineItems.get)
+  private[this] val alreadyDeliveredItems = previousDeliveryNotes flatMap (_.lineItems.get)
+  private[this] lazy val availableLineItems = itemsOrdered filterNot (alreadyDeliveredItems contains _)
+
+  private[this] var selectedLineItems: Seq[LineItem] = Seq.empty
+
+  private[this] def availableAddresses = {
+    val (customerTransactions, customerAddress) = customer match {
+      case Some(cust) => (Transaction where (_.customer eqs cust.id.get) fetch, Address where (_.id eqs cust.businessAddress.get) get)
+      case _ => (Nil, None)
+    }
+
+    val deliveryNotesForCustomer = customerTransactions flatMap (tr => Document where (_.id in tr.documents.get) and (_.documentType eqs DocumentType.DeliveryNote) fetch)
+
+    val deliveredToAddresses = deliveryNotesForCustomer map (_.deliveryAddress.get) sortBy (_.shortName.get)
+
+    if (customerAddress isDefined) customerAddress.get :: deliveredToAddresses else deliveredToAddresses
+  }
+
+  private[this] lazy val addressSelection = (None, "Select Delivery Address") :: (availableAddresses map (addr => (Some(addr), addr.shortName.get)))
 
   override def dispatch = {
     case "render" => render
@@ -27,8 +65,26 @@ class AddEditDelivery extends StatefulValidatingErrorDisplaySnippet with Transac
 
   def render = {
     "#title" #> Text("Create Delivery Note") &
-      renderTransactionDetails()
+      "#scripts" #> JsScripts.accordionScript &
+      renderTransactionDetails() &
+      renderAvailableLineItems(availableLineItems) &
+      renderDocumentTotals() &
+      "#selectOrder" #> styledAjaxObjectSelect(ordersSelection, None, updateAjaxValue((value: Option[Document]) => dataHolder selectedOrder = value)) &
+      "#customerPoRefEntry" #> WiringUI.asText(dataHolder.poReferenceCell) &
+      "#carriage" #> WiringUI.asText(dataHolder.carriage) &
+      "#addressSelect" #> styledAjaxObjectSelect(addressSelection, None, updateAjaxValue((value: Option[Address]) => dataHolder deliveryAddress = value)) &
+      renderEditableAddress()
   }
 
   override val validationItems = Nil
+
+  override def checkBoxSelected(selected: Boolean, line: LineItem) = {
+    selected match {
+      case true => dataHolder.addLineItem(line)
+      case false => dataHolder.removeLineItem(line)
+    }
+    refreshLineItemDisplay()
+  }
+
+  private[this] def textForOrder(order: Document): String = "%s (%s)".format(order.documentNumber, dateString(order.createdOn.get))
 }
