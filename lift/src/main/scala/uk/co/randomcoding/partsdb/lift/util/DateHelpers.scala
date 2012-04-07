2c5a2fcce35fc1129d129152cd824d7b256affd9
randomcoder <randomcoder@randomcoding.co.uk>
2012-04-07 17:37:07 +0100

    re #94: Added string -> date conversion and `joda.DateTime` **->** `java.util.Date` implicit.

--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/DateHelpers.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/DateHelpers.scala
@@ -4,6 +4,7 @@
 package uk.co.randomcoding.partsdb.lift.util
 
 import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 /**
  * Methods to work with Dates & Times
@@ -19,15 +20,22 @@ object DateHelpers {
    *
    * This will render a date as 05/12/2012 for Dec. 5th 2012
    */
-  val dateFormat = "dd/MM/yyyy"
+  final val dateFormat = "dd/MM/yyyy"
 
   /**
    * Get a date as a string, using the default [[uk.co.randomcoding.partsdb.lift.util.DateHelpers#dateFormat]]
    */
   def dateString(date: DateTime) = date.toString(dateFormat)
 
+  def date(dateString: String, format: String = dateFormat): DateTime = {
+    val formatter = DateTimeFormat.forPattern(format)
+    formatter.parseDateTime(dateString).toDate()
+  }
+
   /**
    * Implicit conversion from a `java.util.Date` to a `org.joda.time.DateTime`
    */
   implicit def dateToJoda(date: java.util.Date): DateTime = new DateTime(date)
+
+  implicit def jodaToDate(dateTime: DateTime): java.util.Date = dateTime.toDate()
 }
