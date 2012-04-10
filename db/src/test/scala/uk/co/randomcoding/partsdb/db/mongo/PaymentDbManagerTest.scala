b93872f28a149076ef034d1ee25a02a32dc5f9e9
randomcoder <randomcoder@randomcoding.co.uk>
2012-04-10 08:33:23 +0100

    re #94: Added empty test class for Payment Db Manager

---
+++ db/src/test/scala/uk/co/randomcoding/partsdb/db/mongo/PaymentDbManagerTest.scala
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2012 RandomCoder <randomcoder@randomcoding.co.uk>
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    RandomCoder - initial API and implementation and/or initial documentation
+ */
+package uk.co.randomcoding.partsdb.db.mongo
+
+/**
+ * @author RandomCoder <randomcoder@randomcoding.co.uk>
+ */
+class PaymentDbManagerTest extends MongoDbTestBase {
+  override val dbName = "paymentdbmanagertest"
+
+  // TODO: Write tests
+}
