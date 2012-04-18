5e04e6fed15b440e6debd8c7a53d8d2aa389da6e
randomcoder <randomcoder@randomcoding.co.uk>
2012-04-18 20:46:31 +0100

    Reduced memory uasge to 256M from 512M in sbt startup scripts.

--- sbt.bat
+++ sbt.bat
@@ -1,6 +1,6 @@
 
 set SBT_VER=0.11.2
-set JAVA_OPTS="-d32 -Dfile.encoding=UTF8 -Xmx512M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m"
+set JAVA_OPTS="-d32 -Dfile.encoding=UTF8 -Xmx256M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m"
 set LAUNCH_DIR=%~dp0
 
 java %JAVA_OPTS% -jar "%LAUNCH_DIR%sbt-launch-%SBT_VER%.jar " %*
