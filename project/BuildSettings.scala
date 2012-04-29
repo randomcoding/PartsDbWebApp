1e8ca75af26ff48e4c5cdd2bbea48421c99a3886
randomcoder <randomcoder@randomcoding.co.uk>
2012-04-29 17:15:14 +0100

    Bump version to 0.7.0-SNAPSHOT

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -10,7 +10,7 @@ object BuildSettings {
 
 
   val buildOrganization = "uk.co.randomcoding"
-  val buildVersion = "0.6.0"
+  val buildVersion = "0.7.0-SNAPSHOT"
   val buildScalaVersion = "2.9.1"
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
