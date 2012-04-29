a051a4787352e9c05f610c2a40fe4415ecc13bb9
randomcoder <randomcoder@randomcoding.co.uk>
2012-04-29 13:26:39 +0100

    re #79: Modified address display to provide a section for the Address Id.
    The actual type of display is dependent on the input values to the
    render methods.

--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/snippet/AddressSnippet.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/snippet/AddressSnippet.scala
@@ -5,14 +5,14 @@ package uk.co.randomcoding.partsdb.lift.util.snippet
 
 import scala.io.Source
 import scala.xml.Text
-
 import uk.co.randomcoding.partsdb.core.address.{ AddressParser, Address }
 import uk.co.randomcoding.partsdb.core.util.CountryCodes.countryCodes
 import uk.co.randomcoding.partsdb.lift.util.TransformHelpers._
-
 import net.liftweb.common.Logger
 import net.liftweb.http.SHtml._
 import net.liftweb.util.Helpers._
+import scala.xml.NodeSeq
+import uk.co.randomcoding.partsdb.core.customer.Customer
 
 /**
  * A Snippet that renders and provides an [[uk.co.randomcoding.partsdb.core.address.Address]]
@@ -23,30 +23,57 @@ trait AddressSnippet extends Logger {
 
   var addressText: String
   var addressCountry: String
-  val addressLabel = "Business Address"
+  //val addressLabel = "Business Address"
+  var addressName = ""
 
-  val renderEditableAddress = () => {
-    "#addressLabel" #> Text(addressLabel) &
+  def renderEditableAddress(addressLabel: String = "Business Address", customer: Option[Customer]) = {
+    addressShortName(addressLabel, customer) &
+      "#addressLabel" #> Text(addressLabel) &
       "#billingAddressEntry" #> styledTextArea(addressText, addressText = _) &
       "#billingAddressCountry" #> styledSelect(countryCodes, addressCountry, addressCountry = _)
   }
 
-  val renderReadOnlyAddress = () => {
-    "#addressLabel" #> Text(addressLabel) &
+  def renderReadOnlyAddress(addressLabel: String = "Business Address", customer: Option[Customer]) = {
+    "#addressNameEntry" #> readOnlyAddressName(addressLabel, customer) &
+      "#addressLabel" #> Text(addressLabel) &
       "#billingAddressEntry" #> styledTextArea(addressText, addressText = _, readonly) &
       "#billingAddressCountry" #> styledText(addressCountry, addressCountry = _, readonly)
   }
+
+  private[this] def readOnlyAddressName(addressLabel: String, customer: Option[Customer]) = {
+    addressName = customer match {
+      case Some(cust) => "%s %s".format(cust.customerName.get, addressLabel)
+      case _ => "%s for Unknown Customer".format(addressLabel)
+    }
+
+    Text(addressName)
+  }
+
+  private[this] def addressShortName(addressLabel: String = "Business Address", customer: Option[Customer]) = {
+    (addressLabel, customer) match {
+      case ("Business Address", Some(cust)) => setBusinessAddressNameAndRender(cust)
+      case ("Business Address", None) => hiddenAddressName
+      case (label, None) => "#addressNameEntry" #> Text("New %s:".format(label))
+      case (_, _) => "#addressNameEntry" #> styledAjaxText(addressName, addressName = _)
+    }
+  }
+
+  private[this] def setBusinessAddressNameAndRender(cust: Customer) = {
+    addressName = "%s Business Address".format(cust.customerName.get)
+    "#addressNameEntry" #> Text(addressName)
+  }
+  private[this] def hiddenAddressName = "#addressNameSection" #> <div hidden="true">&nbsp;</div>
 
   def addressFromInput(name: String): Option[Address] = {
     trace("Input address: %s, country: %s".format(addressText, addressCountry))
     val lines = Source.fromString(addressText).getLines toList
     val addressLines = lines.map(_ replaceAll (",", "") trim)
     trace("Generated Address Lines: %s".format(addressLines))
-    val shortName = name
-    val address = addressLines mkString ("", ",", "")
-    debug("Generating Address (%s) from: %s".format(shortName, address))
 
-    (shortName, address, addressCountry) match {
+    val address = addressLines mkString ("", ",", "")
+    debug("Generating Address (%s) from: %s".format(name, address))
+
+    (name, address, addressCountry) match {
       case AddressParser(addr) => {
         debug("Created Address: %s".format(addr))
         Some(addr)
