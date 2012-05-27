2a455351f1fdfa613500e64606a812867f20b624
randomcoder <randomcoder@randomcoding.co.uk>
2012-05-27 21:59:37 +0100

    re #58: Updated line items display to work with part kits as well as parts.

--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/LineItemDisplay.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/LineItemDisplay.scala
@@ -4,14 +4,14 @@
 package uk.co.randomcoding.partsdb.lift.util
 
 import scala.xml.NodeSeq
-
 import org.bson.types.ObjectId
-
 import com.foursquare.rogue.Rogue._
-
 import uk.co.randomcoding.partsdb.core.document.LineItem
 import uk.co.randomcoding.partsdb.core.part.Part
 import uk.co.randomcoding.partsdb.lift.util._
+import uk.co.randomcoding.partsdb.core.part.PartKit
+import net.liftweb.mongodb.record.MongoRecord
+import net.liftweb.mongodb.record.field.ObjectIdPk
 
 /**
  * @author RandomCoder <randomcoder@randomcoding.co.uk>
@@ -26,20 +26,34 @@ object LineItemDisplay extends TabularEntityDisplay {
   override val rowHeadings = List("Line No.", "Part", "Quantity", "Base Price", "Markup", "Total")
 
   override def displayEntity(lineItem: LineItem, editLink: Boolean = false, displayLink: Boolean = false): NodeSeq = {
-    part(lineItem.partId.get) match {
-      case Some(p) => {
-        <td>{ lineItem.lineNumber }</td>
-        <td>{ p.partName.get }</td>
-        <td>{ lineItem.quantity.get }</td>
-        <td>{ "£%.2f".format(lineItem.basePrice.get) }</td>
-        <td>{ "%.0f%%".format(lineItem.markup.get * 100) }</td>
-        <td>{ "£" + totalCost(lineItem, p) }</td>
-      }
+    partOrKit(lineItem.partId.get) match {
+      case Some(p) => row(lineItem, p)
       case _ => emptyRow
     }
   }
 
-  private def totalCost(lineItem: LineItem, part: Part) = "%.2f".format(lineItem.lineCost)
+  private def partOrKit(partId: ObjectId): Option[MongoRecord[_] with ObjectIdPk[_]] = Part where (_.id eqs partId) get match {
+    case Some(p) => Some(p)
+    case _ => PartKit.where(_.id eqs partId).get
+  }
 
-  private def part(partId: ObjectId): Option[Part] = Part where (_.id eqs partId) get
+  private[this] def row(lineItem: LineItem, p: MongoRecord[_] with ObjectIdPk[_]) = {
+    <td>{ lineItem.lineNumber }</td>
+    <td>
+      { partOrKitName(p) }
+    </td>
+    <td>{ lineItem.quantity.get }</td>
+    <td>{ "£%.2f".format(lineItem.basePrice.get) }</td>
+    <td>{ "%.0f%%".format(lineItem.markup.get * 100) }</td>
+    <td>{ "£" + totalCost(lineItem) }</td>
+  }
+
+  private[this] def totalCost(lineItem: LineItem) = "%.2f".format(lineItem.lineCost)
+
+  private[this] def partOrKitName(p: MongoRecord[_] with ObjectIdPk[_]): String = p match {
+    case part: Part => part.partName.get
+    case partKit: PartKit => partKit.kitName.get
+    case _ => "Unknown Object"
+  }
+
 }
