f263a0cc399819bf0788b22281a7bb78bcb9ab7b
randomcoder <randomcoder@randomcoding.co.uk>
2012-06-14 08:25:12 +0100

    re #113: Added supplier id record to line item class
    Also
    * Created apply methods to perform creation in more idiomatic scala
    * Updated ScalaDoc
    * Added throw of exception if C.A.T.9 supplier cannot be found when
    creating a part kit line item.

--- core/src/main/scala/uk/co/randomcoding/partsdb/core/document/LineItem.scala
+++ core/src/main/scala/uk/co/randomcoding/partsdb/core/document/LineItem.scala
@@ -3,12 +3,14 @@
  */
 package uk.co.randomcoding.partsdb.core.document
 
+import com.foursquare.rogue.Rogue._
 import uk.co.randomcoding.partsdb.core.part.{ PartKit, Part }
-import uk.co.randomcoding.partsdb.core.util.MongoFieldHelpers._
-
+import uk.co.randomcoding.partsdb.core.supplier.Supplier
+import uk.co.randomcoding.partsdb.core.util.MongoFieldHelpers.{ doubleFieldToDouble, intFieldToDouble }
 import net.liftweb.record.field._
 import net.liftweb.mongodb.record.{ BsonRecord, BsonMetaRecord }
 import net.liftweb.mongodb.record.field.ObjectIdRefField
+import net.liftweb.common.Logger
 
 /**
  * A line item for documents.
@@ -26,6 +28,7 @@ class LineItem private () extends BsonRecord[LineItem] {
   object quantity extends IntField(this)
   object basePrice extends DoubleField(this)
   object markup extends DoubleField(this)
+  object partSupplier extends ObjectIdRefField(this, Supplier)
 
   /**
    * Calculates the cost of the line item.
@@ -61,9 +64,10 @@ class LineItem private () extends BsonRecord[LineItem] {
 
 }
 
-object LineItem extends LineItem with BsonMetaRecord[LineItem] {
+object LineItem extends LineItem with BsonMetaRecord[LineItem] with Logger {
+
   /**
-   * Create a new `LineItem` but '''do not''' save it to the database
+   * Create a new `LineItem` for a [[uk.co.randomcoding.partsdb.core.part.Part]]
    *
    * @param lineNumber The index of the line
    * @param part The [[uk.co.randomcoding.partsdb.core.part.Part]] that this line item is selling
@@ -71,15 +75,16 @@ object LineItem extends LineItem with BsonMetaRecord[LineItem] {
    * @param basePrice The base price of the part being sold. This is derived from the [[uk.co.randomcoding.partsdb.core.supplier.Supplier]]'s price for the part
    * @param markup The percentage markup to apply to the base price. This derives the total, pre tax, price per part for this line item.
    * This is usually in the range 0.0 to 1.0 where 1.0 is a 100% markup.
+   * @param partSupplier The [[uk.co.randomcoding.partsdb.core.supplier.Supplier]] of the part in the line item.
    *
    * @return The created `LineItem`
    */
-  def create(lineNumber: Int, part: Part, quantity: Int, basePrice: Double, markup: Double): LineItem = {
-    LineItem.createRecord.lineNumber(lineNumber).partId(part.id.get).quantity(quantity).basePrice(basePrice).markup(markup)
+  def apply(lineNumber: Int, part: Part, quantity: Int, basePrice: Double, markup: Double, partSupplier: Supplier): LineItem = {
+    create(lineNumber, part, quantity, basePrice, markup, partSupplier)
   }
 
   /**
-   * Create a new `LineItem` but '''do not''' save it to the database
+   * Create a new `LineItem` for a [[uk.co.randomcoding.partsdb.core.part.PartKit]]
    *
    * @param lineNumber The index of the line
    * @param part The [[uk.co.randomcoding.partsdb.core.part.Part]] that this line item is selling
@@ -89,8 +94,55 @@ object LineItem extends LineItem with BsonMetaRecord[LineItem] {
    * This is usually in the range 0.0 to 1.0 where 1.0 is a 100% markup.
    *
    * @return The created `LineItem`
+   *
+   * @throws LineItemCreationException if the [[uk.co.randomcoding.partsdb.core.supplier.Supplier]] record for '''C.A.T.9''' cannot be found
    */
+  @throws(classOf[LineItemCreationException])
+  def apply(lineNumber: Int, partKit: PartKit, quantity: Int, basePrice: Double, markup: Double): LineItem = {
+    create(lineNumber, partKit, quantity, basePrice, markup)
+  }
+
+  /**
+   * Create a new `LineItem` for a [[uk.co.randomcoding.partsdb.core.part.Part]]
+   *
+   * @param lineNumber The index of the line
+   * @param part The [[uk.co.randomcoding.partsdb.core.part.Part]] that this line item is selling
+   * @param quantity The number of parts in this line item
+   * @param basePrice The base price of the part being sold. This is derived from the [[uk.co.randomcoding.partsdb.core.supplier.Supplier]]'s price for the part
+   * @param markup The percentage markup to apply to the base price. This derives the total, pre tax, price per part for this line item.
+   * This is usually in the range 0.0 to 1.0 where 1.0 is a 100% markup.
+   * @param partSupplier The [[uk.co.randomcoding.partsdb.core.supplier.Supplier]] of the part in the line item.
+   *
+   * @return The created `LineItem`
+   */
+  def create(lineNumber: Int, part: Part, quantity: Int, basePrice: Double, markup: Double, partSupplier: Supplier): LineItem = {
+    LineItem.createRecord.lineNumber(lineNumber).partId(part.id.get).quantity(quantity).basePrice(basePrice).markup(markup).partSupplier(partSupplier.id.get)
+  }
+
+  /**
+   * Create a new `LineItem` for a [[uk.co.randomcoding.partsdb.core.part.PartKit]]
+   *
+   * @param lineNumber The index of the line
+   * @param part The [[uk.co.randomcoding.partsdb.core.part.Part]] that this line item is selling
+   * @param quantity The number of parts in this line item
+   * @param basePrice The base price of the part being sold. This is derived from the [[uk.co.randomcoding.partsdb.core.supplier.Supplier]]'s price for the part
+   * @param markup The percentage markup to apply to the base price. This derives the total, pre tax, price per part for this line item.
+   * This is usually in the range 0.0 to 1.0 where 1.0 is a 100% markup.
+   *
+   * @return The created `LineItem`
+   *
+   * @throws LineItemCreationException if the [[uk.co.randomcoding.partsdb.core.supplier.Supplier]] record for '''C.A.T.9''' cannot be found
+   */
+  @throws(classOf[LineItemCreationException])
   def create(lineNumber: Int, partKit: PartKit, quantity: Int, basePrice: Double, markup: Double): LineItem = {
-    LineItem.createRecord.lineNumber(lineNumber).partId(partKit.id.get).quantity(quantity).basePrice(basePrice).markup(markup)
+    val cat9 = Supplier.where(_.supplierName startsWith "C.A.T.").get
+    cat9 match {
+      case Some(s) => LineItem.createRecord.lineNumber(lineNumber).partId(partKit.id.get).quantity(quantity).basePrice(basePrice).markup(markup).partSupplier(s.id.get)
+      case _ => {
+        val errorMessage = "Failed to find C.A.T.9 Supplier record. Cannot create a Part Kit Line Item"
+        error(errorMessage)
+        throw new LineItemCreationException(errorMessage)
+      }
+    }
   }
 }
