e656d98e1ad7734cf8fa02fc256304677cb5a039
randomcoder <randomcoder@randomcoding.co.uk>
2012-07-07 08:07:00 +0100

    closes #141: Rendering of suppliers is now correct
    I have also made the line item a link to the supplier edit page.

--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/snippet/display/DisplayVehicle.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/snippet/display/DisplayVehicle.scala
@@ -19,21 +19,22 @@
  */
 package uk.co.randomcoding.partsdb.lift.snippet.display
 
-import scala.xml.Text
+import scala.xml.{ Text, NodeSeq }
 
 import com.foursquare.rogue.Rogue._
 
 import uk.co.randomcoding.partsdb.core.part.Part
 import uk.co.randomcoding.partsdb.core.supplier.Supplier
-import uk.co.randomcoding.partsdb.core.system.SystemData
 import uk.co.randomcoding.partsdb.core.util.MongoHelpers._
 import uk.co.randomcoding.partsdb.core.vehicle.Vehicle
 import uk.co.randomcoding.partsdb.lift.util.TransformHelpers._
 
 import net.liftweb.common.Full
+import net.liftweb.http.SHtml.ElemAttr.pairToBasic
+import net.liftweb.http.SHtml._
+import net.liftweb.http.js.JsCmds.Noop
 import net.liftweb.http.{ StatefulSnippet, S }
 import net.liftweb.util.Helpers._
-import net.liftweb.http.SHtml.ElemAttr
 
 /**
  * This snippet displays the details for a vehicle record.
@@ -79,12 +80,19 @@ class DisplayVehicle extends StatefulSnippet {
         case Some(id) => id
         case _ => "No MoD Id"
       }) &
-      "#vehiclePartSuppliers *" #> renderSuppliersForPart(Supplier.where(_.suppliedParts.subfield(_.part) eqs part.id.get).orderAsc(_.supplierName).fetch, part)
+      "#vehiclePartSuppliers *" #> suppliersInfoForPart(part)
   })
 
-  private[this] val supplierEntrySeparator = Text("; ")
+  private[this] def suppliersInfoForPart(part: Part): Seq[NodeSeq] = {
+    val suppliers = Supplier.where(_.suppliedParts.subfield(_.part) eqs part.id.get).orderAsc(_.supplierName).fetch
 
-  private[this] def renderSuppliersForPart(suppliers: List[Supplier], part: Part) = suppliers map (supplier => {
+    suppliers match {
+      case Nil => Seq(span(Text("No Suppliers for part: %s".format(part.partName.get)), Noop))
+      case s => suppliers map (supplierInfoForPart(_, part))
+    }
+  }
+
+  private[this] def supplierInfoForPart(supplier: Supplier, part: Part): NodeSeq = {
     val supplierPartCost = supplier.suppliedParts.get.filter(_.part.get == part.id.get).headOption
     val partId = supplierPartCost match {
       case Some(pc) => pc.supplierPartNumber.get
@@ -95,8 +103,11 @@ class DisplayVehicle extends StatefulSnippet {
       case _ => "Not Supplied" // should not see this
     }
 
-    Text("%s; Part Id: %s; Part Cost: %s".format(supplier.supplierName.get, partId, partCost))
-  })
+    // TODO: Change link target to /app/display/supplier?id= when supplier display is completed
+    plainLink("%s; Part Id: %s; Part Cost: %s".format(supplier.supplierName.get, partId, partCost), "/app/supplier?id=%s".format(supplier.id.get), attrs = List("style" -> "font-weight: normal"))
+  }
+
+  private[this] val supplierEntrySeparator = Text("; ")
 
   private[this] def renderVehiclePdf = vehicle match {
     case Some(v) => v.pdfFile.get.trim match {
