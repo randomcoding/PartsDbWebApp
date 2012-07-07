3bdb49a088f61ca23d7370b38b126a08c6d70cbd
randomcoder <randomcoder@randomcoding.co.uk>
2012-07-07 20:01:11 +0100

    re #120: Changed error display to generate a bullet list of errors with the correct style.

--- lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/snippet/ErrorDisplay.scala
+++ lift/src/main/scala/uk/co/randomcoding/partsdb/lift/util/snippet/ErrorDisplay.scala
@@ -10,38 +10,25 @@ import net.liftweb.http.S
  */
 trait ErrorDisplay {
   private[this] val ERROR_MESSAGE_ELEMENT_ID = "errorMessages"
-  /**
-   * Displays an error message from a snippet
-   *
-   * @param formId The id of the element on the form web page to display the error at.
-   * @param errorMessage The text of the error message to display
-   */
-  @deprecated("Use displayError(String) instead", "0.1")
-  def displayError(formId: String, errorMessage: String): Unit = displayError(errorMessage)
 
   /**
    * Displays an error message from a snippet
    *
    * @param errorMessage The text of the error message to display
    */
-  def displayError(errorMessage: String): Unit = S.error(ERROR_MESSAGE_ELEMENT_ID, errorMessage)
+  def displayError(errorMessage: String): Unit = displayErrors(errorMessage)
 
   /**
    * Display a series of error messages
    */
-  def displayErrors(errorMessages: String*): Unit = errorMessages foreach displayError
+  def displayErrors(errorMessages: String*): Unit = {
+    val errorNodes = errorMessages map (message => <li class="error-message">{ message }</li>)
 
-  /**
-   * Delegate to [[uk.co.randomcoding.partsdb.lift.util.snippet.ErrorDisplay#displayError(String,String)]]
-   *
-   * @param errors One or more `Tuple2[String, String]` which are mapped to the parameters of the delegated method
-   */
-  @deprecated("Use displayErrors(String*) instead", "0.1")
-  def displayError(errors: (String, String)*): Unit = errors foreach { error => displayError(error._2) }
+    S.error(ERROR_MESSAGE_ELEMENT_ID, <ul>{ errorNodes }</ul>)
+  }
 
   /**
    * Remove all errors from the display
    */
-  def clearErrors: Unit = S.error(Nil)
-
+  def clearErrors: Unit = S.error(ERROR_MESSAGE_ELEMENT_ID, Nil)
 }
